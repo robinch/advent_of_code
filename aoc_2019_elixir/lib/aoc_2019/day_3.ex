0def87c1a9f5b82d0c7a9414ea5b846a2dc35b50
robinch <robinch88@gmail.com>
2019-12-13 00:37:34 +0100

    Solution for Day 3 part 2

--- aoc_2019_elixir/lib/aoc_2019/day_3.ex
+++ aoc_2019_elixir/lib/aoc_2019/day_3.ex
@@ -4,29 +4,45 @@ defmodule Aoc2019.Day3 do
     closest_intersection_to_origin(wire_a, wire_b)
   end
 
+  @spec part_2([binary], [binary]) :: integer
+  def part_2(wire_a, wire_b) do
+    shortest_wire_length_to_intersection(wire_a, wire_b)
+  end
+
   defp closest_intersection_to_origin(wire_a, wire_b) do
     wire_a_map = wiring_map(wire_a)
     wire_b_map = wiring_map(wire_b)
 
-    MapSet.intersection(wire_a_map, wire_b_map)
+    intersections(wire_a_map, wire_b_map)
     |> Enum.map(&manhattan_distance/1)
+    |> Enum.min()
+  end
+
+  defp shortest_wire_length_to_intersection(wire_a, wire_b) do
+    wire_a_map = wiring_map(wire_a)
+    wire_b_map = wiring_map(wire_b)
+
+    intersections(wire_a_map, wire_b_map)
+    |> Enum.map(&wire_length_to_intersection(&1, wire_a_map, wire_b_map))
     |> Enum.min()
   end
 
   defp wiring_map(wire) do
     wire = Enum.map(wire, &to_tuple/1)
 
-    Enum.reduce(wire, {MapSet.new(), {0, 0}}, fn {direction, distance}, {map, current_position} ->
-      draw_wiring_line(map, current_position, direction, distance)
+    Enum.reduce(wire, {%{}, {0, 0}, 0}, fn
+      {direction, distance}, {map, current_position, traveled} ->
+        draw_wiring_line(map, current_position, traveled, direction, distance)
     end)
     |> elem(0)
   end
 
-  defp draw_wiring_line(map, current_position, direction, distance) do
-    Enum.reduce(1..distance, {map, current_position}, fn
-      _, {map, current_position} ->
+  defp draw_wiring_line(map, current_position, traveled, direction, distance) do
+    Enum.reduce(1..distance, {map, current_position, traveled}, fn
+      _, {map, current_position, traveled} ->
         new_position = new_position(current_position, direction)
-        {MapSet.put(map, new_position), new_position}
+        traveled = traveled + 1
+        {Map.put(map, new_position, traveled), new_position, traveled}
     end)
   end
 
@@ -34,6 +50,22 @@ defmodule Aoc2019.Day3 do
   defp new_position({x, y}, "L"), do: {x - 1, y}
   defp new_position({x, y}, "U"), do: {x, y + 1}
   defp new_position({x, y}, "D"), do: {x, y - 1}
+
+  defp intersections(wire_a, wire_b) do
+    {shorter_wire, longer_wire} =
+      if Enum.count(wire_a) < Enum.count(wire_b) do
+        {wire_a, wire_b}
+      else
+        {wire_b, wire_a}
+      end
+
+    Enum.filter(shorter_wire, fn {key, _val} -> Map.get(longer_wire, key) != nil end)
+    |> Enum.map(&elem(&1, 0))
+  end
+
+  defp wire_length_to_intersection(intersection, wire_a, wire_b) do
+    Map.get(wire_a, intersection) + Map.get(wire_b, intersection)
+  end
 
   defp manhattan_distance({x, y}), do: abs(x) + abs(y)
 
--- aoc_2019_elixir/test/aoc_2019/day_3_test.exs
+++ aoc_2019_elixir/test/aoc_2019/day_3_test.exs
@@ -2,30 +2,61 @@ defmodule Aoc2019.Day3Test do
   use ExUnit.Case
   alias Aoc2019.Day3
 
-  test "R8,U5,L5,D3 and U7,R6,D4,L4 gives 6" do
-    assert Day3.part_1(["R8", "U5", "L5", "D3"], ["U7", "R6", "D4", "L4"]) == 6
+  describe "part 1" do
+    test "R8,U5,L5,D3 and U7,R6,D4,L4 gives 6" do
+      assert Day3.part_1(["R8", "U5", "L5", "D3"], ["U7", "R6", "D4", "L4"]) == 6
+    end
+
+    test "R75,D30,R83,U83,L12,D49,R71,U7,L72 and U62,R66,U55,R34,D71,R55,D58,R83 gives 159" do
+      assert Day3.part_1(
+               ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"],
+               ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"]
+             ) == 159
+    end
+
+    test "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51 and U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 gives 135" do
+      assert Day3.part_1(
+               ["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"],
+               ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"]
+             ) == 135
+    end
+
+    test "solution" do
+      [wire_a, wire_b] =
+        File.read!("puzzle_inputs/day_3.txt")
+        |> String.split("\n", trim: true)
+        |> Enum.map(&String.split(&1, ","))
+
+      assert Day3.part_1(wire_a, wire_b) == 207
+    end
   end
 
-  test "R75,D30,R83,U83,L12,D49,R71,U7,L72 and U62,R66,U55,R34,D71,R55,D58,R83 gives 159" do
-    assert Day3.part_1(
-             ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"],
-             ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"]
-           ) == 159
-  end
+  describe "part 2" do
+    test "R8,U5,L5,D3 and U7,R6,D4,L4 gives 30" do
+      assert Day3.part_2(["R8", "U5", "L5", "D3"], ["U7", "R6", "D4", "L4"]) == 30
+    end
 
-  test "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51 and U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 gives 135" do
-    assert Day3.part_1(
-             ["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"],
-             ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"]
-           ) == 135
-  end
+    test "R75,D30,R83,U83,L12,D49,R71,U7,L72 and U62,R66,U55,R34,D71,R55,D58,R83 gives 159" do
+      assert Day3.part_2(
+               ["R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72"],
+               ["U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83"]
+             ) == 610
+    end
 
-  test "solution" do
-    [wire_a, wire_b] =
-      File.read!("puzzle_inputs/day_3.txt")
-      |> String.split("\n", trim: true)
-      |> Enum.map(&String.split(&1, ","))
+    test "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51 and U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 gives 135" do
+      assert Day3.part_2(
+               ["R98", "U47", "R26", "D63", "R33", "U87", "L62", "D20", "R33", "U53", "R51"],
+               ["U98", "R91", "D20", "R16", "D67", "R40", "U7", "R15", "U6", "R7"]
+             ) == 410
+    end
 
-    assert Day3.part_1(wire_a, wire_b) == 207
+    test "solution" do
+      [wire_a, wire_b] =
+        File.read!("puzzle_inputs/day_3.txt")
+        |> String.split("\n", trim: true)
+        |> Enum.map(&String.split(&1, ","))
+
+      assert Day3.part_2(wire_a, wire_b) == 21196
+    end
   end
 end
