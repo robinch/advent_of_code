7f499f66fa73a1fb0c8abe3efdf000508d065878
Robin Chowdhury <robinch88@gmail.com>
2023-12-04 19:12:15 +0100

    2023 day 4, part 2 in go

---
+++ aoc_2023_go/day4/part2.go
@@ -0,0 +1,40 @@
+package day4
+
+import (
+	"bufio"
+	"os"
+)
+
+func Part2(filePath string) int {
+	scratchCards := 0
+	file, err := os.Open(filePath)
+	cardCopiesWon := [200]int{}
+
+	if err != nil {
+		panic(err)
+	}
+
+
+	defer file.Close()
+
+
+	scanner := bufio.NewScanner(file)
+
+
+	card := 1
+	for scanner.Scan() {
+		wins := getMatches(scanner.Text())
+		copiesOfCard := 1 + cardCopiesWon[card]
+
+		scratchCards += copiesOfCard
+
+		for i := 0; i < wins; i++ {
+			cardCopiesWon[card + i + 1] += copiesOfCard
+		}
+
+		card++
+	}
+
+
+	return scratchCards
+}
---
+++ aoc_2023_go/day4/part2_test.go
@@ -0,0 +1,12 @@
+package day4
+
+import "testing"
+
+func TestPart2(t *testing.T) {
+	got := Part2("../test_inputs/day4.txt")
+	expect := 30
+
+	if got != expect {
+		t.Errorf("got %d, expected %d", got, expect)
+	}
+}
