0d41436302a315754e4b4a557a1baf12f1bebcd8
Robin Chowdhury <robinch88@gmail.com>
2023-12-15 21:23:21 +0100

    2023 day 14 part 2 in go

--- aoc_2023_go/day14/day14.go
+++ aoc_2023_go/day14/day14.go
@@ -20,29 +20,131 @@ func (p puzzleInput) String() string {
 
 func Part1(filePath string) int {
 	input := parseInput(filePath)
-	fmt.Println(input)
-
-	return calculateLoad(input)
+	tiltNorth(input)
+	return calcLoad(input)
 }
 
-func calculateLoad(input puzzleInput) int {
-	load := 0
-	nrOfRows := len(input)
-	rowLoad := make([]int, len(input[0]))
+func Part2(filePath string) int {
+	input := parseInput(filePath)
+	calc :=calcLoad(input)
 
-	for i := 0; i < len(rowLoad); i++ {
-		rowLoad[i] = nrOfRows
+	seenVals := []int{}
+	lastSeen := map[string]int{}
+
+	n := 0
+	cycleBegin := 0
+
+	for true {
+		tiltAllDirections(input)
+		seenVals = append(seenVals, calc)
+
+		calc = calcLoad(input)
+
+		if i, ok := lastSeen[input.String()]; ok {
+			cycleBegin = i
+			break
+		}
+
+		lastSeen[input.String()] = n
+		n++
 	}
 
-	for row := 0; row < len(input); row++ {
-		for col := 0; col < len(input[row]); col++ {
+	cycles := 1000000000
+	offset := cycleBegin - 1
+	cycle := n - cycleBegin
+
+	index := (cycles - offset) % cycle
+
+	return seenVals[offset + index]
+}
+
+func tiltAllDirections(input puzzleInput) {
+	tiltNorth(input)
+	tiltWest(input)
+	tiltSouth(input)
+	tiltEast(input)
+}
+
+func tiltNorth(input puzzleInput) {
+	tiltV(input, false)
+}
+
+func tiltSouth(input puzzleInput) {
+	tiltV(input, true)
+}
+
+func tiltWest(input puzzleInput) {
+	tiltH(input, false)
+}
+
+func tiltEast(input puzzleInput) {
+	tiltH(input, true)
+}
+
+func tiltV(input puzzleInput, reverse bool) {
+	for col := 0; col < len(input[0]); col++ {
+		occurances := 0
+		start := 0
+		for row := 0; row < len(input); row++ {
 			if input[row][col] == 'O' {
-				load += rowLoad[col]
-				rowLoad[col]--
+				occurances++
 			}
 
 			if input[row][col] == '#' {
-				rowLoad[col] = nrOfRows - (row + 1)
+				shiftCol(input, col, start, row, occurances, reverse)
+				occurances = 0
+				start = row + 1
+			}
+		}
+		shiftCol(input, col, start, len(input), occurances, reverse)
+	}
+}
+
+func shiftCol(input puzzleInput, col, startRow, stopRow, occr int, reverse bool) {
+	for row := startRow; row < stopRow; row++ {
+		if !reverse && row < startRow+occr || reverse && row >= stopRow-occr {
+			input[row][col] = 'O'
+		} else {
+			input[row][col] = '.'
+		}
+	}
+}
+
+func tiltH(input puzzleInput, reverse bool) {
+	for row := 0; row < len(input); row++ {
+		occurances := 0
+		start := 0
+		for col := 0; col < len(input[row]); col++ {
+			if input[row][col] == 'O' {
+				occurances++
+			}
+
+			if input[row][col] == '#' {
+				shiftRow(input[row][start:col], occurances, reverse)
+				occurances = 0
+				start = col + 1
+			}
+		}
+		shiftRow(input[row][start:], occurances, reverse)
+	}
+}
+
+func shiftRow(row []byte, occr int, reverse bool) {
+	for i := 0; i < len(row); i++ {
+		if !reverse && i < occr || reverse && i >= len(row)-occr {
+			row[i] = 'O'
+		} else {
+			row[i] = '.'
+		}
+	}
+}
+
+func calcLoad(input puzzleInput) int {
+	load := 0
+	for row := 0; row < len(input); row++ {
+		for col := 0; col < len(input[row]); col++ {
+			if input[row][col] == 'O' {
+				load += len(input) - row
 			}
 		}
 	}
--- aoc_2023_go/day14/day14_test.go
+++ aoc_2023_go/day14/day14_test.go
@@ -1,6 +1,8 @@
 package day14
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPart1(t *testing.T) {
 	got := Part1("test_input.txt")
@@ -11,3 +13,161 @@ func TestPart1(t *testing.T) {
 	}
 }
 
+
+func TestPart2(t *testing.T) {
+	got := Part2("test_input.txt")
+	want := 64
+
+	if got != want {
+		t.Errorf("Part2() = %d; want %d", got, want)
+	}
+}
+
+func TestTiltNorth(t *testing.T) {
+	puzzleInput := parseInput("test_input.txt")
+	tiltNorth(puzzleInput)
+
+	want := `OOOO.#.O..
+OO..#....#
+OO..O##..O
+O..#.OO...
+........#.
+..#....#.#
+..O..#.O.O
+..O.......
+#....###..
+#....#....
+`
+
+
+	checkTilt(t, puzzleInput, want)
+}
+
+func TestTiltSouth(t *testing.T) {
+	puzzleInput := parseInput("test_input.txt")
+	tiltSouth(puzzleInput)
+
+	want :=`.....#....
+....#....#
+...O.##...
+...#......
+O.O....O#O
+O.#..O.#.#
+O....#....
+OO....OO..
+#OO..###..
+#OO.O#...O
+`
+
+
+	checkTilt(t, puzzleInput, want)
+
+}
+
+func TestTiltWest(t *testing.T) {
+	puzzleInput := parseInput("test_input.txt")
+	tiltWest(puzzleInput)
+
+	want :=`O....#....
+OOO.#....#
+.....##...
+OO.#OO....
+OO......#.
+O.#O...#.#
+O....#OO..
+O.........
+#....###..
+#OO..#....
+`
+
+	checkTilt(t, puzzleInput, want)
+}
+
+func TestTiltEast(t *testing.T) {
+	puzzleInput := parseInput("test_input.txt")
+	tiltEast(puzzleInput)
+
+	want := `....O#....
+.OOO#....#
+.....##...
+.OO#....OO
+......OO#.
+.O#...O#.#
+....O#..OO
+.........O
+#....###..
+#..OO#....
+`
+
+	checkTilt(t, puzzleInput, want)
+}
+
+func TestOneCycle(t *testing.T) {
+	puzzleInput := parseInput("test_input.txt")
+	tiltAllDirections(puzzleInput)
+	
+	want := `.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....
+`
+
+	checkTilt(t, puzzleInput, want)
+}
+
+func TestTwoCycle(t *testing.T) {
+	puzzleInput := parseInput("test_input.txt")
+	tiltAllDirections(puzzleInput)
+	tiltAllDirections(puzzleInput)
+	
+	want := `.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O
+`
+
+	checkTilt(t, puzzleInput, want)
+}
+
+
+func TestThreeCycle(t *testing.T) {
+	puzzleInput := parseInput("test_input.txt")
+	tiltAllDirections(puzzleInput)
+	tiltAllDirections(puzzleInput)
+	tiltAllDirections(puzzleInput)
+	
+	want := `.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O
+`
+
+	checkTilt(t, puzzleInput, want)
+}
+
+func checkTilt(t *testing.T, puzzleInput puzzleInput, want string) {
+	t.Helper()
+	got := puzzleInput.String()
+
+	if got != want {
+		t.Errorf("\ngot\n%s\nwant\n%s", got, want)
+	}
+}
