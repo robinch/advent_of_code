ac480c5ca526f0ffbcdd0fcef8fd54b88add6847
robinch <robinch88@gmail.com>
2025-12-01 21:03:49 +0100

    aoc 2025 day01 part 1 rust

---
+++ aoc_2025/day01_rust/src/main.rs
@@ -0,0 +1,53 @@
+use std::fs;
+
+fn main() {
+    let input = read_input("input/real.txt");
+    println!("Part 1: {}", part1(&input));
+}
+
+fn part1(input: &Vec<(String, i32)>) -> i32 {
+    let mut on_zero = 0;
+    let mut dial = 50;
+
+    for (direction, steps) in input {
+        if direction == "R" {
+            dial = (dial + steps).rem_euclid(100);
+        } else {
+            dial = (dial - steps).rem_euclid(100);
+        }
+
+        if dial == 0 {
+            on_zero += 1;
+        }
+    }
+
+    on_zero
+}
+
+fn read_input(file_path: &str) -> Vec<(String, i32)> {
+    let contents =
+        fs::read_to_string(file_path).expect(&format!("Could not read file {}", file_path));
+    let inputs: Vec<_> = contents
+        .lines()
+        .map(|s| {
+            let (direction, number) = s.split_at(1);
+            let steps = number.parse::<i32>().expect("Could not parse number");
+
+            (direction.to_string(), steps)
+        })
+        .collect();
+
+    inputs
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1_test() {
+        let input = read_input("input/example.txt");
+        let result = part1(&input);
+        assert_eq!(result, 3);
+    }
+}
